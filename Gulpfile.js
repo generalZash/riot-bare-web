807fab8fb5dae50876930480dfd137c5c0c1dd66
Gen Edwards <generalzash@generalzash.com>
2015-06-16 21:43:13 -0400

    ditch light-server in favor of browsersync

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,29 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('styles', function () {
-  gulp.src('sass/**/*.scss')
+// Static server & watch scss/html
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: './index.html'
+    }
+  });
+
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('app/index.html').on('change', browserSync.reload);
+})
+
+// Compile sass into CSS & inject into browsers
+// not sure how this works so go check out www.browsersync.io
+gulp.task('sass', function () {
+  return gulp.src('scss/*.scss')
     .pipe(sass({
       errLogToConsole: true
     }))
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest('app/css/'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('sass/**/*.scss', ['styles']);
-});
+gulp.task('dev', ['serve']);
